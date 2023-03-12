790b33279d38be9d2da72ab618a04c922391f60b
Anh Do <do.dinh.anh@linecorp.com>
2023-03-12 18:57:00 +0700

    Restructure files and remove redundancy

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import FixedSizeList from '../FixedSizeList'
+import FixedSizeList from './components/FixedSizeList'
 
-import styles from './styles.module.css'
+import './App.css'
 
 function App() {
   return (
-    <div className={styles.app}>
-      <section className={styles.section}>
-        <h3 className={styles.heading}>Fixed Size List</h3>
+    <div className="app">
+      <section className="section">
+        <h3 className="heading">Fixed Size List</h3>
         <FixedSizeList
-          className={styles.list}
+          className="list"
           itemCount={1000}
           itemSize={45}
           width={300}
           height={200}
         >
           {({ index, style }) => (
-            <div className={styles.row} style={style}>
+            <div className="row" style={style}>
               Row {index}
             </div>
           )}
         </FixedSizeList>
       </section>
 
-      <section className={styles.section}>
-        <h3 className={styles.heading}>Fixed Size List - Horizontal Direction</h3>
+      <section className="section">
+        <h3 className="heading">Fixed Size List - Horizontal Direction</h3>
         <FixedSizeList
-          className={styles.list}
+          className="list"
           direction="horizontal"
           itemCount={1000}
           itemSize={90}
@@ -33,7 +33,7 @@ function App() {
           height={75}
         >
           {({ index, style }) => (
-            <div className={styles.row} style={style}>
+            <div className="row" style={style}>
               Column {index}
             </div>
           )}
