d72601f7ee83de6b3377fbd900fefbb8c41bfeb1
Anh Do <do.dinh.anh@linecorp.com>
2023-03-15 16:20:25 +0700

    Remove unused files

--- src/hooks/useIsomorphicLayoutEffect.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect, useLayoutEffect } from 'react'
-
-// See: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
-const useIsomorphicLayoutEffect =
-  typeof window !== 'undefined' ? useLayoutEffect : useEffect
-
-export default useIsomorphicLayoutEffect
